83d0a4b4eb2199d68115a0cd35a04c08c45562b3
Stretch Spelman <codingspelman@gmail.com>
2024-02-12 20:10:03 -0700

    fixed code

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,6 @@
-// import models
-const Product = require("./Product");
-const Category = require("./Category");
-const Tag = require("./Tag");
-const ProductTag = require("./ProductTag");
+const router = require("express").Router()
+const apiRoutes = require("./api")
 
+router.use("/api", apiRoutes)
 
-Product.belongsTo(Category, {
-  foreignKey: "category_id",
-});
-
-
-Category.hasMany(Product, {
-  foreignKey: "category_id",
-});
-
-
-Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: "product_id",
-});
-
-Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: "tag_id",
-});
-
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+module.exports = router
